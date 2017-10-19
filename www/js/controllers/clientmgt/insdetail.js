4643d74345390867f4dc8d449c5fbb9b3c56d57d
KomiMoe <995290871@qq.com>
2017-10-19 18:04:26 +0800

    手势密码没commit吗

--- www/js/controllers/clientmgt/insdetail.js
+++ www/js/controllers/clientmgt/insdetail.js
@@ -1,5 +1,25 @@
 angular.module('insdetail.ctrl', [])
-  .controller('InsDetailCtrl', function ($scope) {
+  .controller('InsDetailCtrl', function ($scope,$ionicBackdrop) {
+$scope.terminalList=[];
+$scope.memberList=[];
+for(var i=0;i<20;i++){
+  $scope.terminalList.push({name:'xxxxxxxxxxxxxx大药房',icon:'A',address:'xx路xx街12号'},{name:'xxxxxxx大药房',icon:'B',address:'xxxxx路xxx街12号'},{name:'xxxxxxxxxxxxx大药房',icon:'C',address:'xx路xx街12号'});
+  $scope.memberList.push({name:'王思聪',gender:'m',job:'职务',tel:'13234940596',lock:true},{name:'王思聪',gender:'m',job:'职务',tel:'13234940596',lock:false},{name:'王思聪',gender:'f',job:'职务',tel:'13234940596',lock:false},{name:'王思聪',gender:'f',job:'职务',tel:'13234940596',lock:true});
+}
+    $scope.action = function() {
+      $ionicBackdrop.retain();
+      // $timeout(function() {
+      //   $ionicBackdrop.release();
+      // }, 1000);
+    };
 
+    // Execute action on backdrop disappearing
+    $scope.$on('backdrop.hidden', function() {
+      // Execute action
+    });
 
-  });
+    // Execute action on backdrop appearing
+    $scope.$on('backdrop.shown', function() {
+      // Execute action
+    });
+});
--- www/js/controllers/clientmgt/skusort.js
+++ www/js/controllers/clientmgt/skusort.js
@@ -1,5 +1,16 @@
 angular.module('skusort.ctrl', [])
   .controller('SKUSortCtrl', function ($scope, $ionicListDelegate) {
-    $ionicListDelegate.$getByHandle('my-handle').showReorder(true);
+    // $ionicListDelegate.$getByHandle('my-handle').showReorder(true);
+    $scope.data = {
+      showReorder: true
+    };
+    $scope.items = [];
+    for(var i=0;i<100;i++){
+      $scope.items.push({id:i});
+    }
+    $scope.moveItem = function(item, fromIndex, toIndex) {
+      $scope.items.splice(fromIndex, 1);
+      $scope.items.splice(toIndex, 0, item);
+    };
 
   });
