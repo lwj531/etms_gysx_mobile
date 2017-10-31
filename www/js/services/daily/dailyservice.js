e43c93b4d057c2be4e16b50ae65ef9667c93b47d
xiaoxiao95888 <xiaoxiao95888@gmail.com>
2017-10-31 09:44:21 +0800

    测试发布

--- www/js/services/daily/dailyservice.js
+++ www/js/services/daily/dailyservice.js
@@ -2,6 +2,7 @@ angular.module("daily.srv", ["http.srv"])
   .service("dailysrv", ["httpsrv", function (httpsrv) {
     //保存计划
     this.savePlan = function (model) {
+      //test
       return httpsrv.service("/api/Daily/SaveActivityRouteline", model, "post");
     };
   }
