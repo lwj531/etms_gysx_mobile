84651839443429f15fcf81906afa734dcdf8af92
xiaoxiao95888 <xiaoxiao95888@gmail.com>
2017-11-07 11:06:32 +0800

    修复计划日视图时期滑动

--- www/js/controllers/schedulemgt/schedulemgt.js
+++ www/js/controllers/schedulemgt/schedulemgt.js
@@ -510,19 +510,6 @@ angular.module('schedulemgt.ctrl', ['routesetting.srv', 'daily.srv', 'angularMom
       $scope.week.push(i == 0 ? new Date($scope.weekStartDay.setDate($scope.weekStartDay.getDate() + 0)) : new Date($scope.weekStartDay.setDate($scope.weekStartDay.getDate() + 1)));
       $scope.weekChange[i] = $scope.week[i];
     }
-
-    $scope.NextWeek = function () {
-      for (var n = 0; n < 7; n++) {
-        //week会改变，值赋给weekChange，不能只用一个数组
-        $scope.weekChange[n] = new Date($scope.week[n].setDate($scope.week[n].getDate() + 7));
-      }
-    };
-    $scope.PrevWeek = function () {
-      for (var m = 0; m < 7; m++) {
-        //week会改变，值赋给weekChange，不能只用一个数组
-        $scope.weekChange[m] = new Date($scope.week[m].setDate($scope.week[m].getDate() - 7));
-      }
-    };
     //点击日期时触发
     $scope.clickDate = function (date) {
       $scope.selectedDate = date;
