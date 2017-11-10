9d6d07933adfe5502169f2b9109fda91aa23c4ad
xiaoxiao95888 <xiaoxiao95888@gmail.com>
2017-11-10 10:09:44 +0800

    修复地图重复调用的问题

--- www/js/directive/amap.js
+++ www/js/directive/amap.js
@@ -1,17 +1,42 @@
 angular.module('amap.dt', [])
-.directive("amap", function ($rootScope) {
+.directive("amap", function () {
   return {
     restrict: "AE",
-    replace: true,
+   replace: true,
+    /* scope:{
+      map:'='
+    },*/
     template: "<div style='width: 100%;height: 100%'></div>",
-    link: function ($scope, element, attrs) {
+    /*controller:function($scope,$element){
+      $scope.$on('amap', function (errorType,data) {
+        //已获取数据后执行
+        if(data=="datacompleted"){
+          /!*if($rootScope.map!=null){
+            $rootScope.map.destroy();
+          }*!/
+          //地图初始化
+          $scope.$parent.map = new AMap.Map($element[0],
+            {
+              //center:$scope.center,
+              resizeEnable: true,
+              //zoom: 10,
+              draggable: true,
+              /!*mapStyle: 'amap://styles/b8bc3f4188432ed34aa545312ddf20f9'//样式URL*!/
+            });
+          //$rootScope.map.setFeatures("road");//仅显示道路
+          //通知初始化完成
+          $scope.$emit("amap", "mapcompleted");
+        }
+      });
+    }*/
+    link: function ($scope, element, attrs,controller) {
       $scope.$on('amap', function (errorType,data) {
         //已获取数据后执行
         if(data=="datacompleted"){
           //地图初始化
-          $rootScope.map = new AMap.Map(element[0],
+          $scope.$parent.map = new AMap.Map(element[0],
             {
-              center:$scope.center,
+              //center:$scope.center,
               resizeEnable: true,
               //zoom: 10,
               draggable: true,
@@ -19,7 +44,7 @@ angular.module('amap.dt', [])
             });
           //$rootScope.map.setFeatures("road");//仅显示道路
           //通知初始化完成
-          $rootScope.$broadcast("amap", "mapcompleted");
+          $scope.$emit("amap", "mapcompleted");
         }
       });
     }
